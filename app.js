a749c0334cbd98ccd41da97a66c362905e8e2e6b
piratejas <47509562+piratejas@users.noreply.github.com>
2023-02-17 18:47:09 +0000

    js variables and tipCalculation functions

--- app.js
+++ app.js
@@ -1,3 +1,51 @@
 const billTotal = document.getElementById("bill");
-// const tipPercentage
+const customTipPercentage = document.getElementById("custom");
+const buttons = document.querySelectorAll(".tip-btns button");
 const numOfPeople = document.getElementById("people");
+const tipAmountPerPerson = document.getElementById("tipAmount");
+const totalToPayPerPerson = document.getElementById("totalAmount");
+const resetButton = document.getElementById("resetBtn");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    let tipValue = e.target.innerText;
+    tipValue = tipValue.substr(0, tipValue.length - 1);
+
+    if (billTotal.value === "") {
+      return;
+    }
+    if (numOfPeople.value === "") {
+      numOfPeople.value = 1;
+    }
+
+    calculateTip(
+      parseFloat(billTotal.value),
+      parseInt(tipValue),
+      parseInt(numOfPeople.value)
+    );
+  });
+});
+
+customTipPercentage.addEventListener("blur", (e) => {
+  if (billTotal.value === "") {
+    reset();
+    return;
+  }
+  if (numOfPeople.value === "") {
+    numOfPeople.value = 1;
+  }
+
+  calculateTip(
+    parseFloat(billTotal.value),
+    parseFloat(e.target.value),
+    parseInt(numOfPeople.value)
+  );
+});
+
+function calculateTip(billTotal, tipPercentage, numberOfPeople) {
+    let tipAmount = billTotal * (tipPercentage / 100) / numberOfPeople;
+    let tip = Math.floor(tipAmount * 100) / 100;
+    tip = tip.toFixed(2);
+
+    let totalAmount = 
+}
